bdae844149ee7b2091ad483364f97d10727dad3e
Preeti Singh <singhp1992@gmail.com>
2024-01-08 15:14:51 +0100

    style: alternating colors in the dropdown

--- app/components/dropdown.tsx
+++ app/components/dropdown.tsx
@@ -12,11 +12,17 @@ export default function DropDown({
   selectValue: string;
   setSelectValue: Dispatch<SetStateAction<string>>;
 }) {
+  const alternatingColor = [
+    "bg-light-yellow",
+    "bg-light-green",
+    "bg-light-blue",
+  ];
+
   // bg-yellow-300
   // bg-green-300
   return (
     <div className="rounded-xl shadow-md mt-2 hover:cursor-pointer h-[300px] overflow-scroll ">
-      {data?.map((dog: any, i: string) => (
+      {data?.map((dog: any, i: any) => (
         <div
           key={i}
           tabIndex={1}
@@ -27,7 +33,11 @@ export default function DropDown({
         >
           {/* todo need to change these colors */}
           <div>
-            <div className="w-4 h-4 bg-light-yellow rounded-full flex justify-center items-center" />
+            <div
+              className={`w-4 h-4  rounded-full flex justify-center items-center ${
+                alternatingColor[i % 3]
+              } `}
+            />
           </div>
           <div className="pl-4">
             <p className="font-medium">
